37f11d5ef9b11241153776fdbc09423c59bb74a1
Gijs den Hollander <gijs.den.hollander@ing.com>
2022-12-04 06:39:45 +0100

    Day4 pfff slow

--- src/main/kotlin/day3/day3.kt
+++ src/main/kotlin/day3/day3.kt
@@ -17,7 +17,7 @@ fun main(args: Array<String>) {
         lineOne.forEach { letterArray[it.toInt()] = 1 }
 
         for (letter in lineTwo) {
-            if (letterArray[letter.toInt()] != 0) {
+            if (letterArray[letter.code] != 0) {
                 sum += letterValue(letter)
                 break
             }
@@ -29,10 +29,10 @@ fun main(args: Array<String>) {
     letterArray = IntArray(123)
     var sum2 = 0
     for (lines in inputLines.windowed(3, 3)) {
-        lines[0].forEach { letterArray[it.toInt()] = 1 }
-        lines[1].forEach { if (letterArray[it.toInt()] == 1) letterArray[it.toInt()] = 2 }
+        lines[0].forEach { letterArray[it.code] = 1 }
+        lines[1].forEach { if (letterArray[it.code] == 1) letterArray[it.code] = 2 }
         for (letter in lines[2]) {
-            if (letterArray[letter.toInt()] == 2) {
+            if (letterArray[letter.code] == 2) {
                 sum2 += letterValue(letter)
                 break
             }
@@ -44,8 +44,8 @@ fun main(args: Array<String>) {
 
 // In Kotlin the ASCII value of lowercase alphabets are from 97 to 122. And, the ASCII value of uppercase alphabets are from 65 to 90.
 private fun letterValue(letter: Char): Int {
-    if (letter.toInt() > 90) return letter.toInt() - 96
-    return letter.toInt() - 38 // -64 + 26
+    if (letter.code > 90) return letter.code - 96
+    return letter.code - 38 // -64 + 26
 }
 
 
---
+++ src/main/kotlin/day4/Day4.kt
@@ -0,0 +1,34 @@
+fun main(args: Array<String>) {
+    val input = getResourceAsText("input.txt")
+    val input2 = getResourceAsText("inputExample.txt")
+    val inputLines = input.lines().map { it.split(",") }
+
+    var sum = 0
+    var sum2 = 0
+    for (line in inputLines) {
+        var partOne = line[0].split("-")
+        var partTwo = line[1].split("-")
+        val X1 = partOne[0].toInt()
+        val X2 = partOne[1].toInt()
+
+        val Y1 = partTwo[0].toInt()
+        val Y2 = partTwo[1].toInt()
+
+        if (X1 <= Y1 && X2 >= Y2) {
+            sum += 1
+        } else if (X2 <= Y2 && X1 >= Y1) {
+            sum += 1
+        }
+        if (X2 < Y1 || X1 > Y2) {
+            sum2 += 1
+        }
+    }
+
+    println(sum)
+    println(inputLines.size - sum2)
+}
+
+
+
+
+
