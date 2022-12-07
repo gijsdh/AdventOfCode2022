51e5256a881f5a09693eda579f0321be9d387f78
Gijs den Hollander <gijs.den.hollander@ing.com>
2022-12-07 13:14:19 +0100

    Some refactoring

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -2,85 +2,88 @@ fun main(args: Array<String>) {
     val input = getResourceAsText("input.txt")
     val inputExample = getResourceAsText("inputExample.txt")
     val inputLines = input.lines().drop(1)
-    var folder: Folder = Folder("/", null, 0);
+    var folder = Folder("/", null, 0);
 
     var workFolder = folder
 
     var dirs = mutableListOf<Folder>()
 
     for (line in inputLines) {
-        if (line.startsWith("$")) {
-            if (line.drop(2).equals("cd ..")) {
-                workFolder = workFolder.parentFolder!!
-            } else if (Regex("cd.*").matches(line.drop(2))) {
-                val name = workFolder.name + line.drop(5)
-                val newFolder = Folder(name, workFolder, 0)
-                workFolder.folders.add(newFolder)
-                dirs.add(newFolder)
-                workFolder = newFolder
-            }
-        } else if (line.startsWith("dir")) {
-
-        } else if (Regex("[0-9]{1}").matches(line.subSequence(0, 1))) {
-            var fileNameAndSize = line.split(" ")
-            val size = fileNameAndSize[0].toLong()
-            val name = fileNameAndSize[1]
-
-            val file = File(name, size)
-
-            workFolder.files.add(file)
-            workFolder.size+= size;
-
-            var parent = workFolder.parentFolder
-            while (parent != null) {
-                parent.size += size
-                parent = parent.parentFolder
-            }
-        } else {
-            println("should not happen")
+        when {
+            line.equals("$ cd ..") ->   workFolder = workFolder.parentFolder!!
+            Regex("cd.*").matches(line.drop(2)) -> workFolder = handleFolderMove(workFolder, line, dirs)
+            line.startsWith("dir") -> continue
+            line.startsWith("$ ls") -> continue
+            line[0].isDigit() -> handleFile(line, workFolder)
+            else -> println("should not happen") // sanity check
         }
     }
 
     var sizes :MutableMap<String, Long> = mutableMapOf()
     calculateFolderSize(folder, sizes)
-    println(sizes.filter {it.value <= 100000 }.map { it.value }.sum())
-
-    println(sizes.filter { i -> !dirs.stream().anyMatch{it.size.equals(i.value) }})
-    println(dirs.filter { i -> !sizes.values.stream().anyMatch{it.equals(i.size) }})
 
     val freeSpace = 70000000 - folder.size
     println("Answer A: " + dirs.filter { it.size <= 100000 }.sumOf { it.size })
     println("Answer B: " + dirs.filter { it.size >= 30000000 - freeSpace }.minOf { it.size })
 
+    //This a bit overkill should have calculated it in parsing the input. Was initial set up.
+    //Took me a while to realise name of folder was not unique.
     println("Recursive Answer A: " + sizes.map { it.value }.filter { it <= 100000 }.sum())
     println("Recursive Answer B: " + sizes.map { it.value }.filter { it >= 30000000 - freeSpace }.min())
 }
 
-val sum: (x: Long, y: Long) -> Long = { x, y -> x + y }
+private fun handleFolderMove(workFolder: Folder, line: String, dirs: MutableList<Folder>): Folder {
+    val folderName =
+        workFolder.name + "/" + line.drop(5) // also use the name of the parentFolder as folder names are not unique.
+    val newFolder = Folder(folderName, workFolder, 0)
+    workFolder.folders.add(newFolder)
+    dirs.add(newFolder)
+    return newFolder
+}
+
+private fun handleFile(line: String, workFolder: Folder) {
+    var fileNameAndSize = line.split(" ")
+    val size = fileNameAndSize[0].toLong()
+    val name = fileNameAndSize[1]
+    val file = File(name, size)
+
+    workFolder.files.add(file)
+    workFolder.size += size;
+
+    //size should be updated to folders above as well.
+    var parent = workFolder.parentFolder
+    while (parent != null) {
+        parent.size += size
+        parent = parent.parentFolder
+    }
+}
 
 fun calculateFolderSize(folder: Folder, sizes: MutableMap<String, Long>) {
     var size = folder.files.sumOf { it.size }
     sizes.merge(folder.name, size, sum)
+
+    //size should be updated to folders above as well.
     var workFolder = folder.parentFolder
     while (workFolder != null) {
-        sizes.merge(workFolder!!.name, size, sum)
+        sizes.merge(workFolder.name, size, sum)
         workFolder = workFolder.parentFolder
     }
     folder.folders.forEach { calculateFolderSize(it, sizes) }
 }
 
+val sum: (x: Long, y: Long) -> Long = { x, y -> x + y }
 
 class Folder(var name: String, var parentFolder: Folder? = null, var size: Long) {
     var folders: MutableList<Folder> = mutableListOf()
     var files: MutableList<File> = mutableListOf()
     override fun toString(): String {
-        return "name: " + name + " size " + size
+        return "name: $name size: $size"
     }
 }
 
 class File(var name: String, var size: Long) {
     override fun toString(): String {
-        return "name: " + name + " size: " + size
+        return "name: $name size: $size"
     }
 }
 
