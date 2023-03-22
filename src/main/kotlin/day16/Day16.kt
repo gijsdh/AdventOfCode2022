3035b813e596ad67fa11e9e1012f345242ec4d3d
Gijs den Hollander <gijs.den.hollander@ing.com>
2023-03-22 21:02:20 +0100

    start of day16

--- src/main/kotlin/day16/Day16.kt
+++ src/main/kotlin/day16/Day16.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val input = getResourceAsText("input16.txt")
-    val inputLines = input.lines().map { it.split(Regex(" |,|rate=|;")).filter { it.isNotEmpty() } }
-
+    val inputExample = getResourceAsText("inputExample16.txt")
+    val inputLines = inputExample.lines().map { it.split(Regex(" |,|rate=|;")).filter { it.isNotEmpty() } }
 
     var map = mutableMapOf<String, Valve>()
     for (line in inputLines) {
@@ -14,6 +14,12 @@ fun main(args: Array<String>) {
         valve.valves = valves
         map.put(name, valve)
     }
+
+    // create
+    for() {
+
+    }
+
 
     var t = 0
     while (t < 26) {
@@ -28,10 +34,27 @@ fun main(args: Array<String>) {
 class Valve(var name: String, var flow: Long) {
     var valves: MutableList<String> = mutableListOf()
     override fun toString(): String {
-        return "name: $name size: $flow valves: $valves"
+        return "name: $name flow: $flow valves: $valves"
     }
 }
 
+
+//fun computeDistances(map: MutableMap<String, Valve>): Map<String, List<Pair<Valve, Int>>> {
+//
+//}
+
+
+private fun setAllPairsDistances(distance: Array<IntArray>) {
+    for (k in distance.indices) {
+        for (i in distance.indices) {
+            for (j in distance.indices) {
+                if (distance[i][j] > distance[i][k] + distance[k][j]) {
+                    distance[i][j] = distance[i][k] + distance[k][j]
+                }
+            }
+        }
+    }
+}
 
 
 
